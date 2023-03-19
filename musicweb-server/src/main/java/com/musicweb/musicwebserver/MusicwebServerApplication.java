a7bfbba2f40e2e5135890f6a1a2951cd66c08cc9
AndrzejSasak <andrzej.sasak01@gmail.com>
2023-03-19 22:32:38 +0100

    Initial backend commit

---
+++ musicweb-server/src/main/java/com/musicweb/musicwebserver/MusicwebServerApplication.java
@@ -0,0 +1,13 @@
+package com.musicweb.musicwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicwebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicwebServerApplication.class, args);
+    }
+
+}
