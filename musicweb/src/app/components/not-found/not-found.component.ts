98d4400d71a19d864b7c16b2e0fe364c08706caa
TomekStolarz <tomekstolarz0@gmail.com>
2023-04-02 18:30:49 +0200

    -[feat] added page not found component

--- musicweb/src/app/components/not-found/not-found.component.ts
+++ musicweb/src/app/components/not-found/not-found.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, RouterModule],
 	selector: 'mw-not-found',
 	templateUrl: './not-found.component.html',
 })
