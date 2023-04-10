49360fd644f16b29750dd37a6183b6795b5e2c72
TomekStolarz <tomekstolarz0@gmail.com>
2023-04-10 22:44:34 +0200

    -[feat] added mobile styling

--- musicweb/src/search/search.module.ts
+++ musicweb/src/search/search.module.ts
@@ -4,10 +4,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { SearchTileComponent } from './components/search-tile/search-tile.component';
 import { SharedModule } from 'src/shared/shared.module';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
 	declarations: [SearchComponent, SearchTileComponent],
-	imports: [ReactiveFormsModule, CommonModule, SharedModule],
+	imports: [ReactiveFormsModule, CommonModule, SharedModule, ScrollingModule],
 	exports: [SearchComponent],
 })
 export class SearchModule {}
