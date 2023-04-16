b15a1e5407ad866557ed9b259bbe11fe6048b83f
AndrzejSasak <andrzej.sasak01@gmail.com>
2023-04-16 14:26:05 +0200

    quickfix

--- musicweb-server/src/main/java/com/musicweb/musicwebserver/client/TrackClient.java
+++ musicweb-server/src/main/java/com/musicweb/musicwebserver/client/TrackClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface TrackClient {
 
     @GetMapping("/{trackId}")
-    String getTrackById(@PathVariable Integer trackId);
+    String getTrackById(@PathVariable String trackId);
 
 }
