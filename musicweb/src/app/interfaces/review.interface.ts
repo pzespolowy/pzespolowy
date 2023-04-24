1dd43802092a30c0d756cb5965be2e2a2b3ff67b
TomekStolarz <tomekstolarz0@gmail.com>
2023-04-24 18:02:42 +0200

    -[fix] fixed reviews fields

--- musicweb/src/app/interfaces/review.interface.ts
+++ musicweb/src/app/interfaces/review.interface.ts
@@ -3,6 +3,7 @@ import { ReviewType } from './enums/review-type.enum';
 export interface Review {
 	id: number;
 	grade: number;
-	description?: string;
 	reviewType: ReviewType;
+	description?: string;
+	postedAt?: string;
 }
