e681791b2970cc64e418de0ea0b6b4200cc3e6f9
TomekStolarz <tomekstolarz0@gmail.com>
2023-04-25 20:05:37 +0200

    -[feat] added request for favourites

---
+++ musicweb/src/shared/services/favourite.service.ts
@@ -0,0 +1,72 @@
+import {
+	HttpClient,
+	HttpErrorResponse,
+	HttpParams,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, map, of } from 'rxjs';
+import { ReviewType } from 'src/app/interfaces/enums/review-type.enum';
+import { Response } from '../interfaces/response.interface';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class FavouriteService {
+	apiPath = environment.apiPath;
+
+	constructor(private http: HttpClient) {}
+
+	postFavourite(
+		id: string,
+		reviewType: ReviewType
+	): Observable<Response<string>> {
+		const params = {
+			params: new HttpParams().set(`${reviewType.toLowerCase()}Id`, id),
+		};
+		return this.http
+			.post(
+				`${this.apiPath}/${reviewType.toLowerCase()}s/favourites`,
+				{},
+				params
+			)
+			.pipe(
+				map((x) => ({
+					status: 200,
+					data: 'Succefully added to favourites',
+				})),
+				catchError((x: HttpErrorResponse) => {
+					return of({
+						status: x.error.status,
+						data: x.error.details,
+					});
+				})
+			);
+	}
+
+	deleteFavourite(
+		id: string,
+		reviewType: ReviewType
+	): Observable<Response<string>> {
+		const params = {
+			params: new HttpParams().set(`${reviewType.toLowerCase()}Id`, id),
+		};
+		return this.http
+			.delete(
+				`${this.apiPath}/${reviewType.toLowerCase()}s/favourites`,
+				params
+			)
+			.pipe(
+				map((x) => ({
+					status: 200,
+					data: 'Succefully added to favourites',
+				})),
+				catchError((x: HttpErrorResponse) => {
+					return of({
+						status: x.error.status,
+						data: x.error.details,
+					});
+				})
+			);
+	}
+}
