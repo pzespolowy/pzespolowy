0f498f0f8f640c4dc45c0b6e91cc27d736fe39fb
TomekStolarz <tomekstolarz0@gmail.com>
2023-05-17 22:31:24 +0200

    -[fix] added omnited property

--- musicweb/src/home/services/homepage.service.ts
+++ musicweb/src/home/services/homepage.service.ts
@@ -11,48 +11,49 @@ import { SearchService } from 'src/search/components/services/search.service';
 import { ReviewService } from 'src/shared/services/review.service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class HomepageService {
-  
-  apiPath = environment.apiPath;
+	apiPath = environment.apiPath;
 
 	constructor(
 		private http: HttpClient,
-    private reviewService : ReviewService,
-    private trackDetailsService: TrackDetailsService,
+		private reviewService: ReviewService,
+		private trackDetailsService: TrackDetailsService
 	) {}
 
 	getTracks(): Observable<Track[]> {
 		return this.http
-			.get<{data: TrackResponse[]}>(`${this.apiPath}/tracks/top`)
-			.pipe(map((response) => {
-        return response.data.map((data) => {
-          const { album, release_date, ...trackData } = data;
-					const track = {
-						...trackData,
-						releaseData: release_date,
-						albumId: album.id,
-						albumTitle: album.title,
-						coverSmall: album.cover_small,
-						coverMedium: album.cover_medium,
-						coverBig: album.cover_big,
-						coverXl: album.cover_xl,
-					};
-					return track;
-        })
-				}))
+			.get<{ data: TrackResponse[] }>(`${this.apiPath}/tracks/top`)
+			.pipe(
+				map((response) => {
+					return response.data.map((data) => {
+						const { album, release_date, ...trackData } = data;
+						const track = {
+							...trackData,
+							releaseData: release_date,
+							albumId: album.id,
+							albumTitle: album.title,
+							coverSmall: album.cover_small,
+							coverMedium: album.cover_medium,
+							coverBig: album.cover_big,
+							coverXl: album.cover_xl,
+							type: ReviewType.TRACK,
+						};
+						return track;
+					});
+				})
+			);
 	}
 
-  getTrackRank(track: Track): Observable<Track> {
-    return this.reviewService.getReviews(track.id, ReviewType.TRACK).pipe(
-      map((e) => {
-        return {
-          ...track,
-          rates: e
-        }
-      })
-    );
+	getTrackRank(track: Track): Observable<Track> {
+		return this.reviewService.getReviews(track.id, ReviewType.TRACK).pipe(
+			map((e) => {
+				return {
+					...track,
+					rates: e,
+				};
+			})
+		);
 	}
-
 }
